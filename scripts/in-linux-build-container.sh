9b6dd44030c5481c7a8778234299a5ea2d659f4a
chaser <spuncheshirecat@gmail.com>
2026-09-13 21:02:04 +0700

    Fix persistent build workspace and container caches

--- scripts/in-linux-build-container.sh
+++ scripts/in-linux-build-container.sh
@@ -125,7 +125,7 @@ mounts=(--mount "type=bind,source=$REPO_ROOT,target=$REPO_ROOT")
 if [[ "$workspace" != "$REPO_ROOT/"* ]]; then
   mounts+=(--mount "type=bind,source=$workspace,target=$workspace")
 fi
-env_args=(--env DEPOT_TOOLS_UPDATE=0 --env DEPOT_TOOLS_METRICS=0
+env_args=(--env DEPOT_TOOLS_UPDATE=0 --env DEPOT_TOOLS_METRICS=0 --env HOME=/tmp
           --env "APOSTATE_WORKSPACE=$workspace" --env "APOSTATE_BUILD_IMAGE_ID=$image_id")
 if [[ -n "${APOSTATE_JOBS:-}" ]]; then env_args+=(--env "APOSTATE_JOBS=$APOSTATE_JOBS"); fi
 docker run --rm --init --user "$run_user" --workdir "$REPO_ROOT" \
